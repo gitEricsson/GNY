199151e924dc95087be310137b45504d002a13c2
gitEricsson <ericssonraphael@gmail.com>
2025-12-20 00:45:10 +0100

    Fix/Update: Toast, Typography, FavIcon

--- app/services/[slug]/ServiceClientWrapper.tsx
+++ app/services/[slug]/ServiceClientWrapper.tsx
@@ -1,13 +1,16 @@
-"use client";
+'use client';
 
-import { useContext } from "react";
-import ServiceClient from "./service-client";
-import { LoadingContext } from "../../layout";
+import { useContext } from 'react';
+import ServiceClient from './service-client';
+import { LoadingContext } from '@/components/client-layout-wrapper';
 
 export default function ServiceClientWrapper({ service }: { service: any }) {
   const isLoadingComplete = useContext(LoadingContext);
 
   return (
-    <ServiceClient service={service} isLoadingComplete={isLoadingComplete ?? false} />
+    <ServiceClient
+      service={service}
+      isLoadingComplete={isLoadingComplete ?? false}
+    />
   );
 }
